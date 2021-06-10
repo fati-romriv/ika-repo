8cec10c1da17cf6484f04b38402ae2e19e57d4b9
fati-romriv <84795880+fati-romriv@users.noreply.github.com>
2021-06-10 13:21:33 -0500

    prisionero adaptar

---
+++ proyecto prisionero/Assets/Scripts/InteractuarPrisionero.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractuarPrisionero : MonoBehaviour
+{
+   public Animator animadorPersonaje;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animadorPersonaje.SetBool("Caminando", false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animadorPersonaje.SetBool("Caminando", true);
+        }
+    }
+}
